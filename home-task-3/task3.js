e7d6688bbc8c1d7fe2eb6376d7773397ad5aac25
myweb-hub <70333831+myweb-hub@users.noreply.github.com>
2021-09-09 16:03:10 +0300

    task change

--- home-task-3/task3.js
+++ home-task-3/task3.js
@@ -1,14 +1,19 @@
-function flatten(){
-    if(typeof Array.isArray()){
-        var merged = array.concat.apply([], array); 
-    console.log(merged)
+function uniqueArray(){
+    array.sort((a, b) => a - b);
+    console.log(array);
+    
+    function removeDups(array){
+        var uniqueArray = [];
+        for(i=0; i < array.length; i++){
+            if(uniqueArray.indexOf(array[i])=== -1) {
+                uniqueArray.push(array[i]);
+            }
         }
-    else{
-        return array;
-}
-return flatten();
+        return uniqueArray(array);
     }
-array.forEach(flatten)
-const array = [1, 3, 6, [11, 12],[15,[19,23]]];
+    return removeDups(array);
+    
+}
+    const array =[1,3,4,2,3,1,4,5];
 
     uniqueArray(array); //[1, 1, 2, 3, 3, 4, 4, 5]
