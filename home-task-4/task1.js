062dce7cc37af82af85c87f4f2f4db72177a5e28
myweb-hub <70333831+myweb-hub@users.noreply.github.com>
2021-09-27 18:42:57 +0300

    add script

--- home-task-4/task1.js
+++ home-task-4/task1.js
@@ -1,3 +1,5 @@
+//Make a function (using both recursive and iterative thinking - so actually two separate functions) to calculate the sum of values of such linked list:
+
 const linkedList = {
     value : 1,
     next: {
